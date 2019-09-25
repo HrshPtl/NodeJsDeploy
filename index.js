15f059d8a900b04e895017a0dca988dab6b0a828
harsh <dadhaniaharsh017@gmail.com>
2019-09-25 15:49:05 +0530

    Minor Change

--- index.js
+++ index.js
@@ -11,19 +11,19 @@ const mysql = require('mysql');
 const app = express();
 const port = process.env.PORT || 3000;
 
-//Create Connection
-const conn = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'nodejsdemo'
-});
+// //Create Connection
+// const conn = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'root',
+//   database: 'nodejsdemo'
+// });
 
-//connect to database
-conn.connect((err) =>{
-  if(err) throw err;
-  console.log('Mysql Connected...');
-});
+// //connect to database
+// conn.connect((err) =>{
+//   if(err) throw err;
+//   console.log('Mysql Connected...');
+// });
 
 //set views file
 app.set('views',path.join(__dirname,'views'));
@@ -36,42 +36,40 @@ app.use('/assets',express.static(__dirname + '/public'));
 
 //route for homepage
 app.get('/',(req, res) => {
-  let sql = "SELECT * FROM product";
-  let query = conn.query(sql, (err, results) => {
-    if(err) throw err;
-    res.render('product_view',{
-      results: results
-    });
-  });
+  // let sql = "SELECT * FROM product";
+  // let query = conn.query(sql, (err, results) => {
+    // if(err) throw err;
+    res.render('product_view');
+  // });
 });
 
-//route for insert data
-app.post('/save',(req, res) => {
-  let data = {product_name: req.body.product_name, product_price: req.body.product_price};
-  let sql = "INSERT INTO product SET ?";
-  let query = conn.query(sql, data,(err, results) => {
-    if(err) throw err;
-    res.redirect('/');
-  });
-});
+// //route for insert data
+// app.post('/save',(req, res) => {
+//   let data = {product_name: req.body.product_name, product_price: req.body.product_price};
+//   let sql = "INSERT INTO product SET ?";
+//   let query = conn.query(sql, data,(err, results) => {
+//     if(err) throw err;
+//     res.redirect('/');
+//   });
+// });
 
-//route for update data
-app.post('/update',(req, res) => {
-  let sql = "UPDATE product SET product_name='"+req.body.product_name+"', product_price='"+req.body.product_price+"' WHERE product_id="+req.body.id;
-  let query = conn.query(sql, (err, results) => {
-    if(err) throw err;
-    res.redirect('/');
-  });
-});
+// //route for update data
+// app.post('/update',(req, res) => {
+//   let sql = "UPDATE product SET product_name='"+req.body.product_name+"', product_price='"+req.body.product_price+"' WHERE product_id="+req.body.id;
+//   let query = conn.query(sql, (err, results) => {
+//     if(err) throw err;
+//     res.redirect('/');
+//   });
+// });
 
-//route for delete data
-app.post('/delete',(req, res) => {
-  let sql = "DELETE FROM product WHERE product_id="+req.body.product_id+"";
-  let query = conn.query(sql, (err, results) => {
-    if(err) throw err;
-      res.redirect('/');
-  });
-});
+// //route for delete data
+// app.post('/delete',(req, res) => {
+//   let sql = "DELETE FROM product WHERE product_id="+req.body.product_id+"";
+//   let query = conn.query(sql, (err, results) => {
+//     if(err) throw err;
+//       res.redirect('/');
+//   });
+// });
 
 //server listening
 app.listen(port, () => {
